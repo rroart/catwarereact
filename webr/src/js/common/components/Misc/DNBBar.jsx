7f56289dc7cf61806f12cf0fba65aa5f45fb8f21
Roar Thronæs <rroart@gmail.com>
2019-10-16 10:11:15 +0200

    Add menus.

--- webr/src/js/common/components/Misc/DNBBar.jsx
+++ webr/src/js/common/components/Misc/DNBBar.jsx
@@ -8,6 +8,8 @@ class DNBBar extends PureComponent {
     type : string;
     state = {
 	page: 1,
+	subpage: 1,
+	subsubpage: 1,
 	personnummer: undefined,
 	consentid: '',
 	logon: undefined,
@@ -24,6 +26,8 @@ class DNBBar extends PureComponent {
 	//this.handleChange = this.handleChange.bind(this);
 	this.addMessage = this.addMessage.bind(this);
 	this.buttonClick = this.buttonClick.bind(this);
+	this.dropdownkonto = this.dropdownkonto.bind(this);
+	this.dropdownbetale = this.dropdownbetale.bind(this);
 	this.handleAccountChange = this.handleAccountChange.bind(this);
 	this.handleOtherChange = this.handleOtherChange.bind(this);
 	this.handleMoneyChange = this.handleMoneyChange.bind(this);
@@ -56,7 +60,7 @@ class DNBBar extends PureComponent {
             });
 	});
 	console.log("here");
-	this.state.page = 4
+	this.state.subsubpage = 3
     }
 
     handleSubmit(event) {
@@ -73,6 +77,7 @@ class DNBBar extends PureComponent {
 	    });
 	});
 	this.state.page = 2
+	this.state.subpage = 0
 	/*
 	  Client.search("/dnb/consents/ +  personnummer + "/" + this.state.consentid }, (consentid) => {
 	  this.setState({
@@ -106,18 +111,66 @@ class DNBBar extends PureComponent {
 	});
 	console.log("here")
         console.log(this.state)
-	this.state.page = 3
+	this.state.subsubpage = 2
 
+    }
+
+    dropdownkonto(unknown, event) {
+	console.log(unknown)
+	console.log(event.target.id)
+	console.log(event.target.name)
+        console.log(this.state)
+	console.log("here");
+	this.setState({
+	    page : 2,
+	    subpage : event.target.id,
+	    subsubpage : 1
+	});
+    }
+
+    dropdownbetale(unknown, event) {
+	console.log(event)
+	console.log(event.target.id)
+	console.log(event.target.value)
+        console.log(this.state)
+	console.log("here");
+	this.setState({
+	    page : 3,
+	    subpage : event.target.id,
+	    subsubpage : 1
+	});
     }
 
     render() {
 	console.log("xxx")
 	console.log(this.state.page)
+	console.log(this.state.subpage)
+	console.log(this.state.subsubpage)
 	console.log(this.state.value == undefined)
 	console.log(this.state)
 	console.log(this.state.personnummer == undefined)
 	if (this.state.personnummer != undefined) {
 	    console.log(this.state.personnummer)
+	}
+	let menu;
+	menu = (
+	    <h2>Login</h2>
+	)
+	if (this.state.page != 1) {
+	    menu = (
+		<div>
+		<DropdownButton id="1" title="Konto" onSelect={this.dropdownkonto}>
+		  <MenuItem id="1" name="kontooversikt">Min kontooversikt</MenuItem>
+		  <MenuItem id="2" name="transaksjoner">Siste transaksjoner</MenuItem>
+		</DropdownButton>
+		<DropdownButton id="2" title="Betale og overføre" onSelect={this.dropdownbetale}>
+		  <MenuItem id="1" name="betaleen">Betale til en</MenuItem>
+		  <MenuItem id="2" name="egne">Overføre egne kontoer</MenuItem>
+		  <MenuItem id="3" name="betalingsoversikt">Betalingsoversikt</MenuItem>
+		  <MenuItem id="4" name="betalinger">Utførte betalinger</MenuItem>
+		</DropdownButton>
+		</div>
+	    )
 	}
 	let comp;
 	if (this.state.page == 1) {
@@ -142,121 +195,127 @@ class DNBBar extends PureComponent {
 		</Navbar>
 	    )
 	}
-	if (this.state.page == 2) {
-	    var win = window.open(this.state.logon, '_blank');
-	    win.focus();
-	    const sleep = (milliseconds) => {
-		return new Promise(resolve => setTimeout(resolve, milliseconds))
-	    }
-	    sleep(15000).then(() => {
-		//do stuff
-	    })
-	    console.log("undef");
-	    this.state.logon = undefined;
-	    comp = (
-		<Navbar>
-		  <Navbar.Header>
-		    <Navbar.Brand>
-		      <a href="#home">{this.type}</a>
-		    </Navbar.Brand>
-		  </Navbar.Header>
-		  <Nav>
-		    <NavItem eventKey={3} href="#">
-		      <Button
-			value={this.state.value}
-			placeholder="Enter text"
-			onClick={this.buttonClick}
-			>
-			See accounts
-                      </Button>
-		    </NavItem>
-		  </Nav>
-		</Navbar>
-	    )
-	}
 	if (this.state.page == 3) {
-	    var accts = ConvertToSelect.convert(this.state.accounts);
-	    console.log("here");
-	    comp = (
-		<Navbar>
-		  <Navbar.Header>
-		    <Navbar.Brand>
-		      <a href="#home">{this.type}</a>
-		    </Navbar.Brand>
-		  </Navbar.Header>
-		  <Nav>
-		    <Form>
-		      <FormGroup controlId="form" >
-			<ControlLabel>Working example without validation</ControlLabel>
-			<NavItem eventKey={1} href="#">
-			  Konto
-			  <Select onChange={this.handleAccountChange} options={ accts } />
-			</NavItem>
-			<NavItem eventKey={5} href="#">
-			  Til konto
-			  <FormControl
-			    type="text"
-			    value={this.state.otheraccount}
-			    placeholder="Enter text"
-			    onChange={this.handleOtherChange}
-			    />
-			</NavItem>
-			<NavItem eventKey={6} href="#">
-			  Beløp
-			  <FormControl
-			    type="text"
-			    value={this.state.money}
-			    placeholder="Enter text"
-			    onChange={this.handleMoneyChange}
-			    />
-			</NavItem>
-			<Nav>
-			  <NavItem eventKey={3} href="#">
-			    Send penger
-			    <Button
-			      type="text"
-			      value={this.state.value}
-			      placeholder="Enter text"
-			      onClick={this.submitMoney}
-			      >
-			      Send penger
-			    </Button>
-			  </NavItem>
-			</Nav>
-		      </FormGroup>
-		    </Form>
-		  </Nav>
-		</Navbar>
-	    )
-	}
-	if (this.state.page == 4) {
-	    var win = window.open(this.state.logon, '_blank');
-	    win.focus();
-	    const sleep = (milliseconds) => {
-		return new Promise(resolve => setTimeout(resolve, milliseconds))
+	    if (this.state.subpage == 1) {
+		if (this.state.subsubpage == 1) {
+		    //window.location.href = this.state.logon;
+		    var win = window.open(this.state.logon, '_blank');
+		    win.focus();
+		    const sleep = (milliseconds) => {
+			return new Promise(resolve => setTimeout(resolve, milliseconds))
+		    }
+		    sleep(15000).then(() => {
+			//do stuff
+		    })
+		    console.log("undef");
+		    this.state.logon = undefined;
+		    comp = (
+			<Navbar>
+			  <Navbar.Header>
+			    <Navbar.Brand>
+			      <a href="#home">{this.type}</a>
+			    </Navbar.Brand>
+			  </Navbar.Header>
+			  <Nav>
+			    <NavItem eventKey={3} href="#">
+			      <Button
+				value={this.state.value}
+				placeholder="Enter text"
+				onClick={this.buttonClick}
+				>
+				See accounts
+			      </Button>
+			    </NavItem>
+			  </Nav>
+			</Navbar>
+		    )
+		}
+		if (this.state.subsubpage == 2) {
+		    var accts = ConvertToSelect.convert(this.state.accounts);
+		    console.log("here");
+		    comp = (
+			<Navbar>
+			  <Navbar.Header>
+			    <Navbar.Brand>
+			      <a href="#home">{this.type}</a>
+			    </Navbar.Brand>
+			  </Navbar.Header>
+			  <Nav>
+			    <Form>
+			      <FormGroup controlId="form" >
+				<ControlLabel>Working example without validation</ControlLabel>
+				<NavItem eventKey={1} href="#">
+				  Konto
+				  <Select onChange={this.handleAccountChange} options={ accts } />
+				</NavItem>
+				<NavItem eventKey={5} href="#">
+				  Til konto
+				  <FormControl
+				    type="text"
+				    value={this.state.otheraccount}
+				    placeholder="Enter text"
+				    onChange={this.handleOtherChange}
+				    />
+				</NavItem>
+				<NavItem eventKey={6} href="#">
+				  Beløp
+				  <FormControl
+				    type="text"
+				    value={this.state.money}
+				    placeholder="Enter text"
+				    onChange={this.handleMoneyChange}
+				    />
+				</NavItem>
+				<Nav>
+				  <NavItem eventKey={3} href="#">
+				    Send penger
+				    <Button
+				      type="text"
+				      value={this.state.value}
+				      placeholder="Enter text"
+				      onClick={this.submitMoney}
+				      >
+				      Send penger
+				    </Button>
+				  </NavItem>
+				</Nav>
+			      </FormGroup>
+			    </Form>
+			  </Nav>
+			</Navbar>
+		    )
+		}
+		if (this.state.subsubpage == 3) {
+		    var win = window.open(this.state.logon, '_blank');
+		    win.focus();
+		    const sleep = (milliseconds) => {
+			return new Promise(resolve => setTimeout(resolve, milliseconds))
+		    }
+		    sleep(15000).then(() => {
+			//do stuff
+		    })
+		    console.log("undef");
+		    this.state.logon = undefined;
+		    comp = (
+			<Navbar>
+			  <Navbar.Header>
+			    <Navbar.Brand>
+			      <a href="#home">{this.type}</a>
+			    </Navbar.Brand>
+			  </Navbar.Header>
+			  <Nav>
+			    <NavItem eventKey={3} href="#">
+			      <h2>Betaling registrert med id {this.state.paymentid}</h2>
+			    </NavItem>
+			  </Nav>
+			</Navbar>
+		    )
+		}
 	    }
-	    sleep(15000).then(() => {
-		//do stuff
-	    })
-	    console.log("undef");
-	    this.state.logon = undefined;
-	    comp = (
-		<Navbar>
-		  <Navbar.Header>
-		    <Navbar.Brand>
-		      <a href="#home">{this.type}</a>
-		    </Navbar.Brand>
-		  </Navbar.Header>
-		  <Nav>
-		    <NavItem eventKey={3} href="#">
-		      <h2>Betaling registrert med id {this.state.paymentid}</h2>
-		    </NavItem>
-		  </Nav>
-		</Navbar>
-	    )
 	}
 	return (
 	    <div>
+	      { menu }
 	      { comp }
 	    </div>
 	);
