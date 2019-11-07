df7112e453e4806ba952724fe532d39c1c030fc5
Roar Thronæs <rroart@gmail.com>
2019-11-07 22:57:19 +0100

    For account select.

--- webr/src/js/common/components/util/ConvertToSelect.js
+++ webr/src/js/common/components/util/ConvertToSelect.js
@@ -14,5 +14,21 @@ function convert(array) {
   return myArray;
 }
 
-const ConvertToSelect = { convert };
+function convertAccounts(array) {
+  console.log(array);
+  console.log(typeof array);
+  var myArray = [];
+  for(var i in array) {
+    var acc = array[i];
+    var item = {
+      label: acc.bban,
+      value: acc.bban + " " + acc.name + " Disponibelt: " + acc.authorizedBalance,
+    }
+    myArray.push(item);
+  }
+  console.log(typeof myArray);
+  return myArray;
+}
+
+const ConvertToSelect = { convert, convertAccounts };
 export default ConvertToSelect;
