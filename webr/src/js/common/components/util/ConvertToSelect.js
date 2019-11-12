5c20e6ae05a52ed6ffb28fec6be72a87eed7ed0f
Roar Thronæs <rroart@gmail.com>
2019-11-12 13:19:34 +0100

    Select labels and values.

--- webr/src/js/common/components/util/ConvertToSelect.js
+++ webr/src/js/common/components/util/ConvertToSelect.js
@@ -15,18 +15,20 @@ function convert(array) {
 }
 
 function convertAccounts(array) {
+  array = array.accounts;
   console.log(array);
   console.log(typeof array);
   var myArray = [];
   for(var i in array) {
     var acc = array[i];
     var item = {
-      label: acc.bban,
-      value: acc.bban + " " + acc.name + " Disponibelt: " + acc.authorizedBalance,
+      value: acc.bban,
+      label: acc.bban + " " + acc.name + " Disponibelt: " + acc.authorisedBalance,
     }
     myArray.push(item);
   }
   console.log(typeof myArray);
+  console.log(myArray);
   return myArray;
 }
 
